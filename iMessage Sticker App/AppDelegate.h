6255e847706aa87cb9b9ff2c355fdf6e38dc3b54
Novus Logics <novuslogics@novuss-imac.local>
2017-03-14 15:54:58 +0530

    Initial Commit

---
+++ iMessage Sticker App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iMessage Sticker App
+//
+//  Created by Novus Logics on 14/03/17.
+//  Copyright © 2017 Novus Logics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iMessage Sticker App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iMessage Sticker App
+//
+//  Created by Novus Logics on 14/03/17.
+//  Copyright © 2017 Novus Logics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
